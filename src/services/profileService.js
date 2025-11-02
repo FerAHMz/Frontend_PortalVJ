ba0e04e17d5445b43685458b8e5e912965bf9588
FerAHMw <fernandoh@webtrackgps.net>
2025-11-02 12:28:02 -0600

    Remove duplicate axios import statements in profileImageService and profileService

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 import { getAuthHeaders } from '@/utils/auth.js'
 
-import axios from 'axios';
-
 const API_URL = `${import.meta.env.VITE_API_URL || 'http://localhost:3000'}/api`
 
 export const profileService = {
